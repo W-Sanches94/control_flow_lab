53b4edaaf926a45587eac4675b671f6c9121fce1
W-Sanches94 <117460587+W-Sanches94@users.noreply.github.com>
2022-11-11 03:19:27 +0000

    If,Else If

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,18 +7,25 @@ public class Main {
         String Greeting = "Please Guess a Number Between 1 and 99";
         System.out.println(Greeting);
 
-        Scanner reader = new Scanner(System.in);
+        //User Input?
 
+        Scanner reader = new Scanner(System.in);
         int guessedNumber = reader.nextInt();
 
+
         System.out.println(guessedNumber);
+        System.out.println("Your Guessed Number is" + guessedNumber);
 
+        //If guessedNumber is correct, print "You Win!"
+        //If guessedNumber is incorrect, print "Guess Higher" or "Guess Lower"
 
-
-
-
-
-
+        if (guessedNumber == secretNumber){
+            System.out.println("Congratulations! You Won!");
+        } else if (guessedNumber > secretNumber) {
+            System.out.println("A Little too high, Guess Lower!");
+        } else if (guessedNumber < secretNumber) {
+            System.out.println("A little too low, Guess Higher!");
 
         }
+    }
     }
