0bf6c4d5f11de2c990d2b211d128e07a83e27a9d
W-Sanches94 <117460587+W-Sanches94@users.noreply.github.com>
2022-11-11 02:41:28 +0000

    Greeting

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         int secretNumber = 33;
+        String Greeting = "Please Guess a Number Between 1 and 99";
+        System.out.println(Greeting);
 
         Scanner reader = new Scanner(System.in);
 
@@ -11,6 +14,11 @@ public class Main {
         System.out.println(guessedNumber);
 
 
-    }
 
-}
+
+
+
+
+
+        }
+    }
